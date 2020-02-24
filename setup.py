8117c832e25d1a2f39b2ae24c1d85e0853966d65
Brandon Satrom <brandon@particle.io>
2020-02-24 15:53:13 -0600

    bump to v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-conway",
-    version="0.0.3",
+    version="0.0.4",
     author="Brandon Satrom",
     author_email="bsatrom@gmail.com",
     description="TDD-style implementation of Conway's Game of Life in Python",
